2494fb5fbd4db575345f522f74abee75fa0d1948
wowoowowowow <snitcheditz510@gmail.com>
2025-02-16 22:08:17 +0500

    Create minigames.js

---
+++ JS/minigames.js
@@ -0,0 +1,20 @@
+const minigames = [
+    { name: "BedWars", image: "Images/bedwars.jpg" },
+    { name: "SkyBlock", image: "Images/skyblock.jpg" },
+    { name: "Survival", image: "Images/survival.jpg" },
+    { name: "PvP Arena", image: "Images/pvp.jpg" }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gameContainer = document.getElementById("game-list");
+
+    minigames.forEach(game => {
+        const div = document.createElement("div");
+        div.classList.add("box");
+        div.innerHTML = `
+            <img src="${game.image}" alt="${game.name}">
+            <h3>${game.name}</h3>
+        `;
+        gameContainer.appendChild(div);
+    });
+});
