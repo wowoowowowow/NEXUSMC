c88f0e69f4f56d86ae5a5828638c67e8bd68a6bb
wowoowowowow <snitcheditz510@gmail.com>
2025-02-18 20:50:18 +0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
         navMenu.classList.toggle("active");
     });
 
-    // Close menu when a link is clicked
     document.querySelectorAll(".nav-menu a").forEach(link => {
         link.addEventListener("click", () => {
             hamburger.classList.remove("active");
