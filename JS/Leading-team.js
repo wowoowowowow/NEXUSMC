ecd8f9d9f26e38b23c242c83842b7c5deff95ad6
wowoowowowow <snitcheditz510@gmail.com>
2025-03-04 20:09:32 +0500

    Update Leading-team.js

--- JS/Leading-team.js
+++ JS/Leading-team.js
@@ -1,6 +1,6 @@
 const teamMembers = [
     { name: "Flash", role: "Founder", image: "Images/flash.jpg" },
-    { name: "Dark", role: "Dc-founder", image: "Images/dark.jpg" }
+    { name: "Dark", role: "Dc-founder", image: "Images/dark.jpg" },
     { name: "Mistery", role: "Co-founder", image: "Images/mistery_avatar.png" }
 ];
 
